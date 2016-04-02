111160040ddc57539ddade9d61a0d4c6aadc9350
Alesapin <alesapin@gmail.com>
2016-04-02 14:44:30 +0300

    Trying to parse morphology with turboparser

--- CorefTrainer/Document.cpp
+++ CorefTrainer/Document.cpp
@@ -17,7 +17,6 @@ namespace coref {
                 break;
             }
         }
-
         for(const auto & chain : searchDoc) {
             if (chain.first == "chain") {
                 std::vector<synt::ParsedPharse> currentChain;
@@ -26,7 +25,8 @@ namespace coref {
                     int len = item.second.get<int>("<xmlattr>.ln");
                     std::string type = item.second.get<std::string>("<xmlattr>.str");
                     std::string text = item.second.get<std::string>("cont");
-                    synt::ParsedPharse ph = prs->invokeTurboParser(text);
+                    synt::ParsedPharse ph;
+                    ph.text = text;
                     ph.shift = sh;
                     ph.groupType = type;
                     currentChain.push_back(ph);
@@ -34,15 +34,36 @@ namespace coref {
                 coreferences.push_back(currentChain);
             }
         }
-        for(int i = 0;i<coreferences.size();++i){
-            std::cout <<"Chain: \n";
+        std::vector<std::string> parserRequest;
+        for(int i = 0;i<coreferences.size();++i) {
             for(int j = 0;j<coreferences[i].size();++j){
-                std::cout<<coreferences[i][j]<<"\n";
+                parserRequest.push_back(coreferences[i][j].text);
             }
-
         }
+        std::vector<synt::ParsedPharse> result = prs->invokeTurboParser(parserRequest);
+        for(int i = 0;i<coreferences.size();++i) {
+            for(int j = 0;j<coreferences[i].size();++j){
+                int ind = getSingleDimenIndex(i,j);
+                coreferences[i][j].mainWord = result[ind].mainWord;
+                coreferences[i][j].mainWordNormalForm = result[ind].mainWordNormalForm;
+                coreferences[i][j].morphChars = result[ind].morphChars;
+                coreferences[i][j].sp = result[ind].sp;
+            }
+        }
+    }
 
-
+    int Document::getSingleDimenIndex(int i, int j) {
+        return i*coreferences[i].size() + j;
+    }
+    std::ostream & operator<<(std::ostream &os, const Document &doc) {
+        for(int i = 0;i<doc.coreferences.size();++i){
+            std::cerr<<"Chain: \n";
+            for(int j = 0;j<doc.coreferences[i].size();++j){
+                os<<'\t'<<doc.coreferences[i][j]<<"\n";
+            }
+        }
+        return os;
     }
 }
+
 
--- SyntParser/TurboParser.h
+++ SyntParser/TurboParser.h
@@ -13,6 +13,7 @@
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <iostream>
+#include <limits>
 namespace synt {
     /**
      * All interesting speech parts.
@@ -37,19 +38,32 @@ namespace synt {
     class TurboParser {
     public:
         TurboParser(const std::string &programsPath):path(programsPath){};
-        ParsedPharse invokeTurboParser(const std::string& text) const;
+        std::vector<ParsedPharse> invokeTurboParser(const  std::vector<std::string>& req) const;
     private:
-        bool checkSyntLine(const std::string& line) const;
+
+        ParsedPharse parseOneBlock(const std::string& text, std::ifstream& syntStream, std::ifstream& morphStream) const;
+        bool tryMainWord(ParsedPharse& phrase, std::ifstream& syntStream, std::ifstream& morphStream) const ;
+        bool tryFirstSpeechPart(ParsedPharse& phrase, std::ifstream& syntStream, std::ifstream& morphStream) const;
+        bool tryFirstWord(ParsedPharse& phrase, std::ifstream& syntStream, std::ifstream& morphStream) const;
+        std::string prepareText(const  std::vector<std::string>& req) const;
+        std::size_t getSyntPos(const std::string& syntLine) const;
+        bool isSepLine(const std::string& line) const;
+        void fixStreams(std::ifstream& syntStream, std::ifstream& morphStream) const;
+        bool checkNounOrNproLine(const std::string &line) const;
         void generateLine(ParsedPharse& ph,const std::string& morphLine) const;
         const std::string path;
         static const std::string TURBO_PARSER_OUT;
         static const std::string RUSDEPS_PARSER_OUT;
+        static const std::string SEP_START;
+        static const std::string SEP_END;
         static const std::size_t SYNT_REFERED_NUM_COL;
         static const std::size_t SYNT_SP_COL;
+        static const std::size_t SYNT_WORDFORM_COL;
         static const std::size_t MORPH_CHARS_COL;
         static const std::size_t MORPH_SP_COL;
         static const std::size_t MORPH_WORDFORM_COL;
         static const std::size_t MORPH_NORMALFORM_COL;
+
     };
 }
 
